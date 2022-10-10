e21604af92b3b2fce692320d0865bb02000e2dcf
Junaed-Uddin <junaed.mohammed190@gmail.com>
2022-10-11 01:44:27 +0600

    dynamic route done

--- src/components/QuizTopic/QuizTopic.js
+++ src/components/QuizTopic/QuizTopic.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Checkbox } from "@material-tailwind/react";
 
 const QuizTopic = ({ quizTopic }) => {
     const { id, options, question, correctAnswer } = quizTopic;
-    console.log(quizTopic);
+
     return (
         <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8 border shadow-lg mt-5">
             <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
@@ -11,35 +12,15 @@ const QuizTopic = ({ quizTopic }) => {
                 </p>
             </div>
             <div className="grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-4 sm:grid-cols-2">
-                <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 className">
-                    <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
-                        <h6 className="mb-2 font-semibold leading-5">The doctor said</h6>
-                        <label class="flex items-center">
-                            <Checkbox type="checkbox" class="form-checkbox">
-                                <span class="ml-2">I agree to the <span class="underline">privacy policy</span></span>
-                        </label>
-                    </div>
-                </div>
-                <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400">
-                    <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
-                        <h6 className="mb-2 font-semibold leading-5">Skate ipsum dolor</h6>
-                    </div>
-                </div>
-                <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 className">
-                    <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
-                        <h6 className="mb-2 font-semibold leading-5">
-                            Baseball ipsum dolor
-                        </h6>
+                {
+                    options.map(option => <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 className">
+                        <div className="p-5 h-full flex items-center border border-l-0 rounded-r shadow-sm">
+                            <Checkbox color="blue" />
+                            <p>{option}</p>
+                        </div>
+                    </div>)
+                }
 
-
-                    </div>
-                </div>
-                <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 className">
-                    <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
-                        <h6 className="mb-2 font-semibold leading-5">They urge you</h6>
-
-                    </div>
-                </div>
             </div>
         </div>
     );
