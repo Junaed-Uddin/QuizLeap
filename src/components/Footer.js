3fda8441f3c61b85a2fa96cdd4bc8935cebc5be0
Junaed-Uddin <junaed.mohammed190@gmail.com>
2022-10-11 17:15:26 +0600

    title and fav icon changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className="bg-gray-900 text-white">
-            <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400">
+            <div className="container flex flex-col px-10 pt-4 mx-auto md:pt-8 md:pb-4 lg:flex-row divide-gray-400">
                 <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
                     <li>Topics</li>
                     <li>Statistics</li>
@@ -41,6 +41,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
+            <div className="pb-4 pt-4 sm:pt-0 text-md text-center dark:text-gray-400">© 2022 QuizLeap Co. All rights reserved.</div>
         </footer>
     );
 };
